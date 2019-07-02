1682b6cfe3f38056c27c867a269a29eafbdd683a
Zach  Smith <zdsmith001@gmail.com>
2019-07-02 19:04:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,6 +9,7 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6]]
 
 def won?(board)
+<<<<<<< HEAD
    WIN_COMBINATIONS.detect do |combination|
      if (board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X") ||
         (board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O")
@@ -45,6 +46,11 @@ def winner(board)
       return "O"
     elsif draw?(board)
      nil
+=======
+  WIN_COMBINATIONS.detect do |combination|
+    if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+      return combination.to_a
+>>>>>>> 8c3c0d5d2ff8b1bff160ce28ce8bc375887de7c9
     end
   end
 end
